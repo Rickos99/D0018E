b86b56adddf216d867452b7b0944619fcbb2bdbf
Rickos99 <rickos99@hotmail.com>
2020-11-12 13:18:21 +0100

    Created sample page

---
+++ demo/index.php
@@ -0,0 +1,33 @@
+<?php
+$mysqli = new mysqli("127.0.0.1", "grupp16", "grupp16", "test"); // host, user, pass, db
+
+if ($mysqli -> connect_errorno) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . $mysqli -> connect_errno . PHP_EOL;
+    echo "Debugging error: " . $mysqli -> connect_error . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($mysqli) . PHP_EOL;
+
+$sql = "SELECT * FROM test";
+
+echo "<div>";
+echo "<b>Data</b>";
+echo "<table>";
+
+$returnValue = array();
+if ($result = $mysqli -> query($sql)) {
+    while ($obj = $result -> fetch_object()) {
+        array_push($returnValue, $obj);
+        echo "<tr><td>" . $obj->idtest . "</td></tr>";
+    }
+    $result -> free_result();
+}
+echo "</table>";
+echo "</div>";
+echo "<hr>";
+var_dump($returnValue);
+ 
+$mysqli -> close();
