0101c3ce79d7047139b700fe883177481222e598
Rickos99 <rickos99@hotmail.com>
2020-11-12 13:51:40 +0100

    Create generateDB.sql

---
+++ sql/generateDB.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA `STORE` ;
+USE `STORE`;
+
+CREATE TABLE `PRODUCTS` (
+	`prod_id` int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`name` varchar(256) NOT NULL,
+    `description` text,
+    `price` float NOT NULL,
+    `vat` float NOT NULL,
+    `balance` int NOT NULL
+);
+
+CREATE TABLE `USERS` (
+	`uid` int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`full_name` varchar(128),
+    `hashed_pwd` varchar(32) NOT NULL,
+    `address` varchar(256),
+    `phone` varchar(20),
+    `email` varchar(256)
+);
