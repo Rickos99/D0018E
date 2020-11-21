deeea9c0ab4fd689bb0cfab868a2fb2bd79fbe1c
Rickos99 <rickos99@hotmail.com>
2020-11-21 11:31:45 +0100

    Ändrat teckenkodning i databas
    Ändrat teckenkodning i databas till UTF-8 enligt #7 .

--- demo/must/php/sql/db.conn.php
+++ demo/must/php/sql/db.conn.php
@@ -2,6 +2,7 @@
 
 function getDBConnection(){
     $mysqli = new mysqli("127.0.0.1", "grupp16", "grupp16", "STORE"); // host, user, pass, db
+    $mysqli->set_charset("utf8");
     if ($mysqli -> connect_errno) {
         echo "Error: Unable to connect to MySQL." . PHP_EOL;
         echo "Debugging errno: " . $mysqli -> connect_errno . PHP_EOL;
