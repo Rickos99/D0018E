1de7e314a96e2684c23abad0e41368c3aaf1b93e
Axelkarnebro <36851484+Axelkarnebro@users.noreply.github.com>
2020-11-23 14:34:45 +0100

    Update generateDB.sql

--- sql/generateDB.sql
+++ sql/generateDB.sql
@@ -13,8 +13,16 @@ CREATE TABLE `PRODUCTS` (
 CREATE TABLE `USERS` (
 	`uid` int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	`full_name` varchar(128),
-    `hashed_pwd` varchar(32) NOT NULL,
+    `hashed_pwd` varchar(255) NOT NULL,
     `address` varchar(256),
     `phone` varchar(20),
     `email` varchar(256)
+);
+
+CREATE TABLE `ORDERS` (
+	`order_id` int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `uid` int UNSIGNED,
+    `status` text,
+    `created_at` datetime NOT NULL,
+    FOREIGN KEY(uid) REFERENCES USERS(uid)
 );
