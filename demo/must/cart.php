d1fcf3ae4152a0f33c79c33d7698a3a14509929e
Rickos99 <rickos99@hotmail.com>
2020-11-25 11:47:44 +0100

    Funktion för lägga till produkt i kundvagn

---
+++ demo/must/cart.php
@@ -0,0 +1,7 @@
+<?php
+
+include "php/debugSettings.php";
+require_once "php/sql/addProductToCart.php";
+
+addProductToCart(8, 8, 1);
+pprint("Success");
---
+++ demo/must/php/sql/addProductToCart.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once "db.conn.php";
+
+function addProductToCart(int $uid, int $pid, int $quantity){
+    $mysqli = getDbConnection();
+    
+    $sql = "SELECT cart_id FROM `STORE`.`SHOPPING_CARTS` WHERE uid = ?";
+    $stmtGetCart = $mysqli -> prepare($sql);
+    $stmtGetCart -> bind_param("i", $uid);
+    $stmtGetCart -> execute();
+    $stmtGetCart -> store_result();
+    $stmtGetCart -> bind_result($cartId);
+    $stmtGetCart -> fetch();
+
+    if($stmtGetCart -> num_rows == 0){
+        $dateTimeNow = date("Y-m-d h:i:s");
+
+        $sql = "INSERT INTO `STORE`.`SHOPPING_CARTS` (uid, changed_at) VALUES (?, ?)";
+        $stmtCreateCart = $mysqli -> prepare($sql) or die("(".__LINE__ .") Error: " . $mysqli -> error);
+        $stmtCreateCart -> bind_param("is", $uid, $dateTimeNow);
+        $stmtCreateCart -> execute();
+        $mysqli->close();
+
+        return addProductToCart($uid, $pid, $quantity);
+    }
+
+    $sql = "SELECT quantity FROM `STORE`.`CART_ITEMS` WHERE cart_id = ?";
+    $stmtGetProduct = $mysqli -> prepare($sql);
+    $stmtGetProduct -> bind_param("i", $cartId);
+    $stmtGetProduct -> execute();
+    $stmtGetProduct -> store_result();
+
+    if($stmtGetProduct -> num_rows == 0){
+        $sql = "INSERT INTO CART_ITEMS (quantity, product_id, cart_id) VALUES (?, ?, ?)";
+    } else {
+        $sql = "UPDATE `STORE`.`CART_ITEMS` SET quantity = ? WHERE product_id = ? AND cart_id = ?";
+    }
+    $stmtChangeProductQuantity = $mysqli -> prepare($sql);
+    $stmtChangeProductQuantity -> bind_param("iii", $quantity, $pid, $cartId);
+    $stmtChangeProductQuantity -> execute();
+
+    $mysqli -> close();
+}
