a118a4b66dab0ebf328c6dd7a44a3782d9c75142
SimonHotDog <48484552+SimonHotDog@users.noreply.github.com>
2020-12-03 15:01:48 +0100

    Early version of Checkout

--- beställning/checkout.php
+++ beställning/checkout.php
@@ -4,39 +4,45 @@
 require_once "../php/userSession.php";
 $user = new userSession(true, [0,1,2]);
 
+$mysqli = new mysqli("127.0.0.1", "grupp16", "grupp16", "STORE");
+
 //Get Session variables
 $uid = $_SESSION["uid"];
 
-echo $uid;
 //Query for SHOPPING_CARTS
 $a = "SELECT cart_id FROM SHOPPING_CARTS WHERE uid=".$uid;
 
+/* OLDER CODE, Kept for reference
 //Query for CART_ITEMS
 $b = "SELECT product_id FROM CART_ITEMS WHERE cart_id=(".$a.")";
 
 //Query for Quantity
-$c = "SELECT quantity FROM CART_ITEMS WHERE cart_id=(".$a.")";
+$quantityQuery = "SELECT quantity FROM CART_ITEMS WHERE cart_id=(".$a.")";
 
 //Query for PRODUCTS
-$d = "SELECT name, description, price WHERE prod_id=(".$b.")";
+$productQuery = "SELECT name, price WHERE prod_id=(".$b.")";
+*/
 
-echo $d;
+$finalQuery = "SELECT PRODUCTS.name, PRODUCTS.price, CART_ITEMS.quantity FROM PRODUCTS INNER JOIN CART_ITEMS ON PRODUCTS.prod_id=CART_ITEMS.product_id WHERE cart_id=(".$a.")";
 
-/*
+$r = mysqli_query($mysqli, $finalQuery);
+
+
 while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
     $values[] = array(
         'name' => $row['name'],
-        'address' => $row['address'],
-        'content' => $row['content']
+        'price' => $row['price'],
+        'quantity' => $row['quantity']
     );
 }
-*/
+
 
 ?>
 <html>
 <head>
 	<title>BESTTEST</title>
 	<link rel="stylesheet" type="text/css" href="order.css">
+    <meta charset="UTF-8">
 </head>
 <body>
 <header>
@@ -45,21 +51,17 @@ while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
 <article class="left">
 	<h2>Your Cart</h2>
 <table style="width:100%">
-  <tr>
-    <th>Product name</th>
-    <th>Price</th>
-    <th>Quantity</th>
-  </tr>
-  <tr>
-    <td>Tårta</td>
-    <td>50</td>
-    <td>2</td>
-  </tr>
-  <tr>
-    <td>Hörlurar</td>
-    <td>1000</td>
-    <td>1</td>
-  </tr>
+<?php
+foreach($values as $v){
+    echo '
+    <tr>
+        <td>'.$v['name'].'</td>
+        <td>'.$v['price'].'</td>
+        <td>'.$v['quantity'].'</td>
+    </tr>
+    ';
+}
+?>
 </table> 
 </article>
 
