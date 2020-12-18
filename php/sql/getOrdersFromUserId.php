01436cc9239660bd72df5b2d66c40c55a13c5efa
Rickard Bemm <rickos99@hotmail.com>
2020-12-18 13:48:18 +0100

    Förberett för #37
    Föreberett hämtning av en kunds alla lagda ordrar samt ändrat support vy
    som påverkats av ändringen av ändrat funktionsresultat.

--- php/sql/getOrdersFromUserId.php
+++ php/sql/getOrdersFromUserId.php
@@ -5,16 +5,24 @@ require_once "db.conn.php";
 function getOrdersFromUserId(int $uid) : array {
     $mysqli = getDBConnection();
 
-    $sql = "SELECT order_id FROM STORE.ORDERS WHERE uid = ?;";
+    $sql = "SELECT O.order_id, O.status, O.created_at, sum(OI.price*(1 + OI.vat/100)*OI.quantity) FROM STORE.ORDERS as O 
+               INNER JOIN STORE.ORDER_ITEMS as OI ON O.order_id = OI.order_id
+               WHERE uid = ?
+               GROUP BY O.order_id;";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("i", $uid);
     $stmt->execute();
     $stmt->store_result();
-    $stmt->bind_result($orderId);
+    $stmt->bind_result($orderId, $status, $created_at, $totalPriceWithVat);
     
     $orders = [];
     while($stmt->fetch()){
-        array_push($orders, $orderId);
+        $order = new stdClass();
+        $order->status = $status;
+        $order->order_id = $orderId;
+        $order->created_at = $created_at;
+        $order->totalPriceWithVat = $totalPriceWithVat;
+        array_push($orders, $order);
     }
 
     $stmt->close();
