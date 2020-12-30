aa8405c674808eca8ecf2ad07933058c3f18ea19
Rickard Bemm <rickos99@hotmail.com>
2020-12-30 16:44:34 +0100

    Ändrat SQL-frågor (#35)
    - Ändrat struktur i funktion för att hämta alla produkter från databasen.
    - Lagt till en valfri parameter till funktionerna för att få med status
    på önskelistan för given produkt.

--- php/sql/getAllProducts.php
+++ php/sql/getAllProducts.php
@@ -2,26 +2,26 @@
 
 require_once "db.conn.php";
 
-function getAllProducts() {
-    $mysqli = getDBConnection();
-    $sql = "SELECT P.prod_id, P.name, P.description, P.price, P.vat, P.balance, AVG(R.rating) as rating, COUNT(R.rating) as ratings FROM PRODUCTS as P
-              LEFT JOIN REVIEWS as R ON P.prod_id = R.product_id
-              GROUP BY P.prod_id";
+function getAllProducts(int $uid = -1) {
+    $sql = "SELECT P.prod_id, P.name, P.description, P.price, P.vat, P.balance, AVG(R.rating) as rating, COUNT(R.rating) as ratings, (SELECT W.uid IS NOT NULL) as onWishlist FROM PRODUCTS as P
+                LEFT JOIN REVIEWS as R ON P.prod_id = R.product_id
+                LEFT JOIN WISHLIST_ITEMS as W ON P.prod_id = W.pid and W.uid = $uid
+                GROUP BY P.prod_id";
     
+    $mysqli = getDBConnection();
     $returnValue = array();
-    if ($result = $mysqli -> query($sql)) {
-        while ($obj = $result -> fetch_object()) {
-            $obj -> priceInclVat = (1 + $obj->vat/100) * $obj->price;
-            $obj -> rating = empty($obj->rating) ? 0 : round((float)$obj->rating, 1);
+    if ($result = $mysqli->query($sql)) {
+        while ($obj = $result->fetch_object()) {
+            $obj->onWishlist = boolval($obj->onWishlist);
+            $obj->priceInclVat = (1 + $obj->vat/100) * $obj->price;
+            $obj->rating = empty($obj->rating) ? 0 : round((float)$obj->rating, 1);
 
             array_push($returnValue, $obj);
         }
         $result -> free_result();
     } else {
-        echo "Error: Unable to execute query"; ;
-        echo "Debugging errno: " . $mysqli->errno . PHP_EOL;
-        echo "Debugging error: " . $mysqli->error . PHP_EOL;
+        throw new Error($mysqli->error, $mysqli->errno);
     }
-    $mysqli -> close();
+    $mysqli->close();
     return $returnValue;
 }
--- php/sql/getProduct.php
+++ php/sql/getProduct.php
@@ -2,58 +2,59 @@
 
 require_once "db.conn.php";
 
-function getProduct(int $productId) {
+function getProduct(int $pid, int $uid = -1) {
     $mysqli = getDBConnection();
-    $sqlProduct = "SELECT P.prod_id, P.name, P.description, P.price, P.vat, P.balance, AVG(R.rating) as rating, COUNT(R.rating) as ratings FROM PRODUCTS as P
-	                 LEFT JOIN REVIEWS as R ON P.prod_id = R.product_id
-                     WHERE prod_id = ?
-                     GROUP BY P.prod_id";
+    $sqlProduct = "SELECT P.prod_id, P.name, P.description, P.price, P.vat, P.balance FROM PRODUCTS as P WHERE prod_id = $pid";
+    $sqlWishlist = "SELECT W.pid FROM WISHLIST_ITEMS as W WHERE W.pid = $pid and W.uid = $uid";
     $sqlReviews = "SELECT R.title, R.comment, R.rating, R.recommends, R.created_at, U.full_name FROM REVIEWS as R
 	                 LEFT JOIN USERS as U ON R.uid = U.uid
-                     WHERE R.product_id = ? AND R.comment IS NOT NULL
+                     WHERE R.product_id = $pid AND R.comment IS NOT NULL
                      ORDER BY R.created_at DESC";
 
-    $stmt = $mysqli -> prepare($sqlReviews);
-    $stmt -> bind_param("i", $productId);
-    $stmt -> execute();
-    $stmt -> bind_result($title, $comment, $rating, $recommends, $created_at, $username);
-    $stmt -> store_result();
-
-    $reviews = [];
-    while($stmt -> fetch()){
-        $review = new stdClass();
-        $review -> title = $title;
-        $review -> comment = $comment;
-        $review -> rating = $rating;
-        $review -> recommends = $recommends;
-        $review -> created_at = $created_at;
-        $review -> username = $username;
-        array_push($reviews, $review);
-    }
-    $stmt -> close();
-
-    $stmt = $mysqli -> prepare($sqlProduct);
-    $stmt -> bind_param("i", $productId);
-    $stmt -> execute();
-    $stmt -> bind_result($prod_id, $name, $description, $price, $vat, $balance, $rating, $ratings);
-    $stmt -> store_result();
-    
-    $returnValue = new stdClass();
-    if($stmt -> num_rows == 0){
-        $returnValue = null;
+    // Product information
+    $product = new stdClass();
+    $resultSetProduct = $mysqli->query($sqlProduct);
+    if($resultSetProduct->num_rows === 0){
+        return null;
     } else {
-        $stmt -> fetch();
-        $returnValue -> prod_id = $prod_id;
-        $returnValue -> name = $name;
-        $returnValue -> description = $description;
-        $returnValue -> price = $price;
-        $returnValue -> vat = $vat;
-        $returnValue -> balance = $balance;
-        $returnValue -> rating = empty($rating) ? 0 : round((float)$rating, 1);
-        $returnValue -> ratings = $ratings;
-        $returnValue -> reviews = $reviews;
+        $result = $resultSetProduct->fetch_object();
+
+        $product->vat = $result->vat;
+        $product->name = $result->name;
+        $product->price = $result->price;
+        $product->prod_id = $result->prod_id;
+        $product->balance = $result->balance;
+        $product->description = $result->description;
     }
 
-    $mysqli -> close();
-    return $returnValue;
+    // Reviews
+    $reviews = [];
+    $totalNumberOfStars = 0;
+    $resultSetReviews = $mysqli->query($sqlReviews);
+    while ($result = $resultSetReviews->fetch_object()) {
+        $review = new stdClass();
+        $review->title = $result->title;
+        $review->comment = $result->comment;
+        $review->rating = $result->rating;
+        $review->recommends = $result->recommends;
+        $review->created_at = $result->created_at;
+        $review->username = $result->full_name;
+        array_push($reviews, $review);
+
+        $totalNumberOfStars += $result->rating;
+    }
+
+    // Wishlist
+    $resultSetWishlist = $mysqli->query($sqlWishlist);
+    $productIsonWishlist = $resultSetWishlist->num_rows === 1;
+    $resultSetWishlist->close();
+
+    // Assign last properties to product
+    $product->rating = empty($reviews) ? 0 : round((float)($totalNumberOfStars/count($reviews)), 1);
+    $product->ratings = count($reviews);
+    $product->reviews = $reviews;
+    $product->onWishlist = $productIsonWishlist;
+
+    $mysqli->close();
+    return $product;
 }
