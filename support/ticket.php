52891ae2c32e2753fa14aae740ea7865d850b2c0
Rickard Bemm <rickos99@hotmail.com>
2021-01-04 22:43:56 +0100

    Lagt till enkla kommentarer

--- support/ticket.php
+++ support/ticket.php
@@ -16,26 +16,32 @@ include_once "../php/mustacheHelpers.php";
 $user = new userSession(true, [0, 1]);
 $user->isSupportStaff = $user->role === 1 ? true : false;
 
+// Check if post request
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     $ticketId = intval($_POST['ticketId']);
+
+    // Close an open ticket
     if($_POST['action'] === 'closeTicket' && $user->role === 1){
         if(!updateSupportTicketStatus($ticketId, 1, $user->uid)){
             die("ERROR: The ticket status could not be updated");
         } 
 
+    // Reopen an already closed ticket
     } else if($_POST['action'] === 'openTicket' && $user->role === 1){
         if(!updateSupportTicketStatus($ticketId, 0, $user->uid)){
             die("ERROR: The ticket status could not be updated");
         }
 
+    // Add an response to a ticket
     } else if($_POST['action'] === 'addResponse'){
         $ticketOwner = getOwnerOfSupportTicket($ticketId);
-        if(!($uid === $ticketOwner || $user->role === 1)){
+        if(!($uid !== $ticketOwner || $user->role !== 1)){
             header("HTTP/1.1 400 Bad Request");
             exit();
         }
 
+        // Create a new response object
         $newResponse = new stdClass();
         $newResponse->uid = $user->uid;
         $newResponse->body = substr(trim($_POST['responseBody']), 0, 1000);
@@ -44,6 +50,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             die("ERROR: Could not save ticketresponse to database");
         }
 
+    // Open a new ticket
     } else if ($_POST['action'] === 'addTicket'){
         $newTicket = new stdClass();
         $newTicket->body = substr(trim($_POST['body']), 0, 2000);
@@ -61,11 +68,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         die("ERROR: Unkown actiontype!");
     }
 
+    // Redirectback to same page to avoid 
     header("location: ".$_SERVER['REQUEST_URI']);
     exit();
 }
 
+// get ticketid from request
 $ticketId = $_GET['t'];
+
+// Display ticket by ticketid if found
 if(!empty($ticketId)){
     $ticket = getSupportTicket($ticketId);
     if($user->role === 0 && $user->uid !== $ticket->owner || empty((array)$ticket)){
@@ -75,12 +86,16 @@ if(!empty($ticketId)){
         "user" => $user,
         "ticket" => $ticket
     ]);
+
+// If user is support staff, display all open tickets
 } else if($user->role === 1) {
     renderTemplate("displaySupportTicketsStaff", [
         "user" => $user,
         "tickets" => getSupportTickets(),
         "fnTimeSince" => $fnTimeSinceShort
     ]);
+
+// Else user is customer and all his/hers tickets will be displayed
 } else {
     renderTemplate("displaySupportTicketsCustomer", [
         "user" => $user,
